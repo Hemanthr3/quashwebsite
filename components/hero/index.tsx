ef97f3a099afefabe0a3b669b08facc92aca35d3
Hemanthr1 <hemanth@quashbugs.com>
2023-09-20 12:01:59 +0530

    hero section completed

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -4,23 +4,33 @@ import { Button } from "../ui/button";
 
 const Hero = () => {
   return (
-    <div className="relative flex flex-col gap-5 items-center">
-      <div className="absolute left-0 right-0 top-[200px] bottom-0 w-full bg-[url('/Gradient.svg')]" />
-      <div className=" pt-[260px] px-[242px] pb-[109px] w-full text-center items-center flex flex-col gap-[72px]">
-        <div className="flex flex-col gap-[28px] text-center items-center">
+    <div className="relative flex flex-col md:gap-5 items-center">
+      <div className="absolute left-0 right-0 md:top-[200px] bottom-0 w-full bg-[url('/Gradient.svg')]" />
+      <div className=" pt-[132px] px-[18px] pb-[45px] md:pt-[260px] md:px-[242px] md:pb-[109px] w-full text-center items-center flex flex-col gap-[45px] md:gap-[72px]">
+        <div className="flex flex-col gap-5 md:gap-[28px] text-center items-center">
           <Image
             src="/hero-image.svg"
             alt="hero-image"
             width={1028}
             height={166}
-            className=""
+            className="hidden md:flex"
           />
-          <span className="text-[24px] text-[#ECECEE] font-[400]">
+          <div>
+            <span className="text-[32px] font-[600] md:text-[24px] text-[#ECECEE] md:font-[600] w-full text-center leading-normal">
+              Mobile Testing
+              <br />
+              should not be slow
+              <br />
+              and tangled
+            </span>
+          </div>
+
+          <span className="text-[16px] w-[252px] font-[300] md:text-[24px] text-[#ECECEE] md:font-[400] md:w-full">
             We’re on a mission to make it smooth and simple
           </span>
         </div>
         <Button
-          className="text-[#000000] text-[24px] font-[600] bg-[#FFFFFF] hidden px-4 py-[14px] rounded-[100px] h-[58px] md:flex z-10"
+          className="text-[#000000] text-[16px] md:text-[24px] font-[600] bg-[#FFFFFF] px-4 py-3 md:py-[14px] rounded-[100px] h-[58px] flex z-10 leading-5 md:leading-normal"
           variant="outline"
         >
           Get Started for Free
@@ -31,8 +41,17 @@ const Hero = () => {
         alt="hero"
         width={1078}
         height={600}
-        className="z-10"
+        className="z-10 hidden md:flex"
       />
+      <div className="w-full flex justify-end md:hidden">
+        <Image
+          src="/hero-mob.svg"
+          alt="hero"
+          width={345}
+          height={303}
+          className="z-10 flex md:hidden pb-[73px]"
+        />
+      </div>
     </div>
   );
 };
