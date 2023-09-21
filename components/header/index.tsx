d804923422614a136a56d104c80b73c4a49c3100
Hemanthr1 <hemanth@quashbugs.com>
2023-09-21 14:47:59 +0530

    menu added for mobile view

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -9,12 +9,33 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Button } from "../ui/button";
-import { List } from "@/lib/icons";
+import { List, X } from "@/lib/icons";
+import { LoaderDialog, LoaderDialogContent } from "../ui/overlay-loader";
 
 const navButtons = ["Home", "Upcoming", "Team"];
 
 const Header = () => {
   const [selected, setSelected] = useState("Home");
+  const [open, setOpen] = useState(false);
+
+  const mobileNav = [
+    {
+      label: "Home",
+      value: "home",
+    },
+    {
+      label: "UpComing",
+      value: "upcoming",
+    },
+    {
+      label: "Team",
+      value: "team",
+    },
+    {
+      label: "Sign in",
+      value: "signin",
+    },
+  ];
 
   return (
     <div className="header fixed top-0 py-2 px-4 w-full flex justify-between items-center md:px-[120px] md:py-6 z-50">
@@ -69,8 +90,53 @@ const Header = () => {
         >
           Get Started for Free
         </Button>
-        <List size={24} color="#F7F8F8" className="flex md:hidden" />
+        <List
+          size={24}
+          color="#F7F8F8"
+          className="flex md:hidden"
+          onClick={() => {
+            setOpen(true);
+          }}
+        />
       </div>
+      {open && (
+        <LoaderDialog open={true}>
+          <LoaderDialogContent className="flex justify-center items-start p-0 h-full">
+            <div className="w-full bg-black p-4 flex flex-col gap-1">
+              <div className="flex justify-between items-center p-2">
+                <span className="text-[12px] text-[#FFFFFF99] font-[500] leading-5">
+                  MENU
+                </span>
+                <X
+                  size={16}
+                  color="#FFFFFF99"
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                />
+              </div>
+              {mobileNav.map((nav, index) => (
+                <div
+                  className="flex justify-start items-center p-2"
+                  key={index}
+                >
+                  <span className="text-[12px] text-white font-[500] leading-5">
+                    {nav.label}
+                  </span>
+                </div>
+              ))}
+              <div className="flex justify-between items-center p-2">
+                <Button
+                  className="text-[#000000] text-[12px] font-[600] leading-[15px] bg-[#FFFFFF] px-2 py-[6px] rounded-[100px] h-[28px] md:hidden"
+                  variant="outline"
+                >
+                  Get Started for free
+                </Button>
+              </div>
+            </div>
+          </LoaderDialogContent>
+        </LoaderDialog>
+      )}
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,6 @@ export default function Home() {
   return (
     <div className="relative min-h-screen">
       <Header />
-
       <main>
         <Hero />
         <Comparision />
