3d981831549db3a18d7b69fa08a5211c2dc270f5
Hemanthr1 <hemanth@quashbugs.com>
2023-09-22 15:08:08 +0530

    head tag updated

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Main, NextScript } from "next/document";
+import Head from "next/head";
 
 export default function Document() {
   return (
@@ -8,8 +9,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap"
           rel="stylesheet"
         ></link>
+        <title>🐞 Quash - Streamlining QA Processes 🚀</title>
       </Head>
-      <title>🐞 Quash - Streamlining QA Processes 🚀</title>
       <body>
         <Main />
         <NextScript />
